d166c95451946e306ff48405ad6f08cb1383a976
Austin Graham <austiingraham@gmail.com>
2024-01-10 02:10:20 -0500

    added route to create a new category using async/await

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,8 +35,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(201).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
