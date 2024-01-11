f233112f7aed55ae5530ce99cb0ba4a06c2c1646
Austin Graham <austiingraham@gmail.com>
2024-01-11 01:59:54 -0500

    added route to delete a tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,8 +64,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// delete a tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedTag) {
+      res.status(404).json({ message: 'Tag not found' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Tag deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
